bf6282140102c76437543e4b0db15e6acce6f252
Josh Gulledge <jgulledge19@gmail.com>
2023-05-25 17:38:34 -0600

    Set up index page to load on main page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import App from "./App"
 import Show from "./pages/Show"
+import Index from "./pages/Index"
 // import NewJob from "./pages/NewJob"
 // import Update from "./pages/Update"
 // import UpdateJob from "./pages/UpdateJob"
@@ -13,6 +14,7 @@ export const store = new RootStore()
 const router = createBrowserRouter(createRoutesFromElements(
   <>
       <Route path="/" element={<App/>}>
+      <Route path="" element={<Index/>}/>
           <Route path="show/:id" element={<Show/>} />
           {/* <Route path="create" element={<NewJob/>} />
           <Route path="newdl/:id" element={<CreateDl/>} />
