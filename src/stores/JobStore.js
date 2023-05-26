938d2bf9ef531a6d7941f2da39be06fa5e7a1264
Josh Gulledge <jgulledge19@gmail.com>
2023-05-25 18:46:31 -0600

    Made create job function

--- src/stores/JobStore.js
+++ src/stores/JobStore.js
@@ -12,6 +12,7 @@ export default class JobStore{
         makeObservable(this, {
             _jobs: observable,
             fetchJobs: action,
+            createJob: action,
         })
     }
     
@@ -31,5 +32,22 @@ export default class JobStore{
         const response = await fetch(`${url}/job/`)
         const body = await response.json()
         return body
+    }
+
+    async createJob(e){
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const newJob = {
+            name: formData.get("name"),
+            number: formData.get("number"),
+        }
+        await fetch(`${url}/job/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newJob)
+        })
+        await this.init()
     }
 }
