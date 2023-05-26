339ec0ddbc4125eec9bdef7e351f14079654e34e
Josh Gulledge <jgulledge19@gmail.com>
2023-05-25 18:47:40 -0600

    Added in new job to routes

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import {createBrowserRouter, createRoutesFromElements, Route} from "react-router
 import App from "./App"
 import Show from "./pages/Show"
 import Index from "./pages/Index"
-// import NewJob from "./pages/NewJob"
+import NewJob from "./pages/NewJob"
 // import Update from "./pages/Update"
 // import UpdateJob from "./pages/UpdateJob"
 // import CreateDl from "./pages/CreateDl"
@@ -16,8 +16,8 @@ const router = createBrowserRouter(createRoutesFromElements(
       <Route path="/" element={<App/>}>
       <Route path="" element={<Index/>}/>
           <Route path="show/:id" element={<Show/>} />
-          {/* <Route path="create" element={<NewJob/>} />
-          <Route path="newdl/:id" element={<CreateDl/>} />
+          <Route path="create" element={<NewJob/>} />
+          {/* <Route path="newdl/:id" element={<CreateDl/>} />
           <Route path="edit/:id/:dlId" element={<Update/>} />
           <Route path="editjob/:id" element={<UpdateJob/>}/> */}
       </Route>
