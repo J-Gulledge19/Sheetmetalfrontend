5dd4c64ea2f964a33cffdc1ace3c7634b686db34
Josh Gulledge <jgulledge19@gmail.com>
2023-06-07 18:35:45 -0600

    Commented out unused code for now

--- src/pages/CreateDl.js
+++ src/pages/CreateDl.js
@@ -1,6 +1,6 @@
 import {observer} from 'mobx-react'
-import { store } from '../router';
-import { Form, useNavigate } from "react-router-dom"
+// import { store } from '../router';
+// import { Form, useNavigate } from "react-router-dom"
 
 function NewDl() {
     return (<h1>newdl form</h1>)
