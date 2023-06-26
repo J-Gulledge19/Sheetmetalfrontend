4877bc3b0f942013f120d683214c1acf602e3f50
Josh Gulledge <jgulledge19@gmail.com>
2023-06-26 17:22:10 -0600

    Set up downloads map function

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,56 +6,56 @@ import { store } from '../router'
 const Show = () => {
     const { id } = useParams()
     const selectedJob = store.jobStore._jobs.find(job => job.id === parseInt(id))
-    console.log(selectedJob)
+    console.log(selectedJob.downloads)
     return (
-        <div>
-            <h1 class="secondh1"> {selectedJob.name} </h1>
-        <div class="button">
-            <Link className='link' to={`/newdl/${selectedJob.id}`}>New Download</Link>
-        </div>
-        
-        {/* <table> 
-                
-                <h2>{dl.dlName}</h2>
+            <>
+                    <h1 class="secondh1"> {selectedJob.name} </h1>
                 <div class="button">
-                <Link className='link' to={`/job/dl/${job.id}/${dl._id}/edit`}>Edit</Link>
-                <form class="delete" action={`/job/dl/${job.id}/${dl._id}?_method=DELETE`} method="POST">
-                    <input type="submit" value="Delete"/>
-                </form>
+                    <Link className='link' to={`/newdl/${selectedJob.id}`}>New Download</Link>
                 </div>
-            <tr>
-                <th>Active</th>
-                <th>Job Number</th>
-                <th>Date Turned In</th>
-                <th>Date Due</th>
-            </tr>
-            <tr>
-              <td>dl.active ? "Active" : "Not Active"</td>
-              <td> {job.number} </td>
-              <td> {dl.dateTurnedIn} </td>
-              <td> {dl.dateDue}</td>
-            </tr>
-        </table>
+                {selectedJob.downloads.map(dl => (
+                <><table>
+                        <h2>{dl.dlName}</h2>
+                        <div class="button">
+                            <Link className='link' to={`/job/dl/${selectedJob.id}/${dl._id}/edit`}>Edit</Link>
+                            <form class="delete" action={`/job/dl/${selectedJob.id}/${dl._id}?_method=DELETE`} method="POST">
+                                <input type="submit" value="Delete" />
+                            </form>
+                        </div>
 
-        <table class="tabletwo">
-            <h3>{dl.dlName} Weights</h3>
-            <tr>
-                <th>Galvinized</th>
-                <th>Stainless</th>
-                <th>Black Iron</th>
-                <th>Paint Lock</th>
-                <th>Aluminum</th>
-            </tr>
-            
-            <tr>
-              <td> {dl.weight.galv} lbs</td>
-              <td> {dl.weight.ss} lbs</td>
-              <td> {dl.weight.blackIron} lbs </td>
-              <td> {dl.weight.pl} lbs </td>
-              <td> {dl.weight.alum} lbs</td>
-            </tr>
-        </table>      */}
-        </div>
+                        <tr>
+                            <th>Active</th>
+                            <th>Job Number</th>
+                            <th>Date Turned In</th>
+                            <th>Date Due</th>
+                        </tr>
+                        <tr>
+                            <td>{dl.active ? "Active" : "Not Active"}</td>
+                            <td> {selectedJob.number} </td>
+                            <td> {dl.dateTurnedIn} </td>
+                            <td> {dl.dateDue}</td>
+                        </tr>
+                    </table><table class="tabletwo">
+                            <h3>{dl.dlName} Weights</h3>
+                            <tr>
+                                <th>Galvinized</th>
+                                <th>Stainless</th>
+                                <th>Black Iron</th>
+                                <th>Paint Lock</th>
+                                <th>Aluminum</th>
+                            </tr>
+
+                            <tr>
+                                <td> {dl.weight.galv} lbs</td>
+                                <td> {dl.weight.ss} lbs</td>
+                                <td> {dl.weight.blackIron} lbs </td>
+                                <td> {dl.weight.pl} lbs </td>
+                                <td> {dl.weight.alum} lbs</td>
+                            </tr>
+                        </table></>
+                ))
+                }     
+            </>
     )
 
 }
