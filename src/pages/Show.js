6421ac8cbf302ffa60e4d9b3476200a545ec4370
Josh Gulledge <jgulledge19@gmail.com>
2023-07-07 15:17:47 -0600

    Updated show page for the new data model

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,3 +1,5 @@
+import { AiFillEdit } from 'react-icons/ai';
+import { RiDeleteBin5Line } from 'react-icons/ri';
 import {observer} from 'mobx-react'
 import { Link, useParams } from 'react-router-dom'
 import { store } from '../router'
@@ -13,50 +15,54 @@ const Show = () => {
                 <div class="button">
                     <Link className='link' to={`/newdl/${selectedJob.id}`}>New Download</Link>
                 </div>
-                {selectedJob.downloads.map(dl => (
                     <>
-                        <h2>{dl.dlName}</h2>
+                        {/*
                         <div class="button">
-                            <Link className='link' to={`/job/dl/${selectedJob.id}/${dl._id}/edit`}>Edit</Link>
-                            <form class="delete" action={`/job/dl/${selectedJob.id}/${dl._id}?_method=DELETE`} method="POST">
-                                <input type="submit" value="Delete" />
-                            </form>
-                        </div>
+                        <Link className='link' to={`/job/dl/${selectedJob.id}/${dl._id}/edit`}>Edit</Link>
+                        <form class="delete" action={`/job/dl/${selectedJob.id}/${dl._id}?_method=DELETE`} method="POST">
+                        <input type="submit" value="Delete" />
+                        </form>
+                    </div> */}
                     <table>
 
                         <tr>
                             <th>Active</th>
+                            <th>Area</th>
                             <th>Job Number</th>
+                            <th>Received By</th>
                             <th>Date Turned In</th>
                             <th>Date Due</th>
+                            <th>Cut Out</th>
+                            <th>Number of Fittings</th>
+                            <th>Amount of Duct</th>
+                            <th>Weight</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
                         </tr>
+                    {selectedJob.downloads.map(dl => (
                         <tr>
                             <td>{dl.active ? "Active" : "Not Active"}</td>
+                            <td> {dl.dlName} </td>
                             <td> {selectedJob.number} </td>
+                            <td> {dl.receivedBy} </td>
                             <td> {dl.dateTurnedIn} </td>
                             <td> {dl.dateDue}</td>
+                            <td>{dl.cutOut ? "Yes" : "No"}</td>
+                            <td>{dl.fittings}</td>
+                            <td>{dl.duct}</td>
+                            <td>{dl.weight}</td>
+                            <Link className="link" to={`/edit/${selectedJob.id}/${dl.id}`}>
+                            <td className='show-edit'><AiFillEdit/></td>
+                        </Link>
+                            <td className='deletebutton' onClick={async () => {
+                                await store.jobStore.deletedlAction(dl.id)
+                            }}>
+                                <RiDeleteBin5Line className='deletebutton' />
+                            </td> 
                         </tr>
-                    </table>
-                    <h2>Weight</h2>
-                    <table class="tabletwo">
-                            <tr>
-                                <th>Galvinized</th>
-                                <th>Stainless</th>
-                                <th>Black Iron</th>
-                                <th>Paint Lock</th>
-                                <th>Aluminum</th>
-                            </tr>
-
-                            <tr>
-                                <td> {dl.weight.galv} lbs</td>
-                                <td> {dl.weight.ss} lbs</td>
-                                <td> {dl.weight.blackIron} lbs </td>
-                                <td> {dl.weight.pl} lbs </td>
-                                <td> {dl.weight.alum} lbs</td>
-                            </tr>
-                        </table></>
                 ))
                 }     
+                    </table></>
             </>
     )
 
