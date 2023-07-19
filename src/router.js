fc939325f7cd865c37645a1ab129816fd110000e
Josh Gulledge <jgulledge19@gmail.com>
2023-07-19 16:37:46 -0600

    Added in update download route

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import App from "./App"
 import Show from "./pages/Show"
 import Index from "./pages/Index"
 import NewJob from "./pages/NewJob"
-// import Update from "./pages/Update"
+import UpdateDl from "./pages/UpdateDl"
 // import UpdateJob from "./pages/UpdateJob"
 import CreateDl from "./pages/CreateDl"
 import { RootStore } from "./stores/RootStore"
@@ -18,8 +18,8 @@ const router = createBrowserRouter(createRoutesFromElements(
           <Route path="show/:id" element={<Show/>} />
           <Route path="create" element={<NewJob/>} />
           <Route path="newdl/:id" element={<CreateDl/>} />
-          {/* <Route path="edit/:id/:dlId" element={<Update/>} />
-          <Route path="editjob/:id" element={<UpdateJob/>}/> */}
+          <Route path="edit/:id/:dlId" element={<UpdateDl/>} />
+          {/* <Route path="editjob/:id" element={<UpdateJob/>}/> */}
       </Route>
   </>
 ))
