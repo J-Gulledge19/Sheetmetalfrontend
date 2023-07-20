33fc10392ff84eefe33f2e7941dd702e593f46f0
Josh Gulledge <jgulledge19@gmail.com>
2023-07-20 16:48:13 -0600

    Created edit and delete buttons in index for job

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,8 @@
 import {observer} from 'mobx-react'
 import { store } from '../router';
 import { Link } from 'react-router-dom';
+import { AiFillEdit } from 'react-icons/ai';
+import { RiDeleteBin5Line } from 'react-icons/ri';
 
 
 const Index = () => {
@@ -14,6 +16,8 @@ const Index = () => {
                 <th>Job Name</th>
                 <th>Job Number</th>
                 <th>Total Weight</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
         
             {store.jobStore._jobs.map(job => (
@@ -23,6 +27,14 @@ const Index = () => {
                 </Link></td>
                 <td>{job.number}</td>
                 <td></td>
+                <Link className="link" to={`/editjob/${job.id}`}>
+                        <td><div className='show-edit'><AiFillEdit/></div></td>
+                    </Link>
+                        <td className='deletebutton' onClick={async () => {
+                            await store.jobStore.deleteAction(job.id)
+                        }}>
+                            <RiDeleteBin5Line className='deletebutton' />
+                        </td> 
             </tr>
             ))
             }   
